751d57d6465ce42602f3db496d0db93bc00c5162
Vivek Kadam <144525243+kadamvivek5708@users.noreply.github.com>
2025-06-27 16:17:29 +0530

    Update nullpro.py

--- 2/nullpro.py
+++ 2/nullpro.py
@@ -16,11 +16,18 @@ if filee is not None:
   if(st.button('Generate')):
       if(option == 'Mean'):
         file['Column1'].fillna((file['Column1'].mean()), inplace=True)
+        file['Column2'].fillna((file['Column2'].mean()), inplace=True)
+        file['Column3'].fillna((file['Column3'].mean()), inplace=True)
 
       if(option == 'Min'):
         file['Column1'].fillna((file['Column1'].min()), inplace=True)
+        file['Column2'].fillna((file['Column2'].min()), inplace=True)
+        file['Column3'].fillna((file['Column3'].min()), inplace=True)
 
       if(option == 'Max'):
         file['Column1'].fillna((file['Column1'].max()), inplace=True)
+        file['Column2'].fillna((file['Column2'].max()), inplace=True)
+        file['Column3'].fillna((file['Column3'].max()), inplace=True)
+
 
       st.dataframe(file)
